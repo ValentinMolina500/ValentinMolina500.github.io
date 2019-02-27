d69af8de0fccc66de411e91f264700e71b210e4d
Valentin Molina <valentinmolina83@gmail.com>
2019-02-26 23:59:39 -0800

    added cleverbot

---
+++ cleverbot/main.js
@@ -0,0 +1,46 @@
+var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+var synth = window.speechSynthesis;
+var voices = synth.getVoices();
+var url =   "https://www.cleverbot.com/getreply";
+var API_KEY = "CC8srKyiHqx77hURUxkjduSJnCw";
+var cs = '';
+var textContainer = document.getElementById('mainText');
+var button = document.getElementById('speakCuh');
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 5;
+button.onclick = function() {
+    recognition.start();
+}
+recognition.onresult = function(event) {
+    var response = event.results[0][0].transcript
+    console.log('You said: ', response);
+    var text = document.createElement("p")
+    text.innerHTML = response;
+    textContainer.appendChild(text);
+
+    var input = encodeURIComponent(response);
+
+    $.ajax({
+        url: url,
+        data: {"input": input, "key": API_KEY, "cs": cs},
+        dataType: "json",
+        success: ProcessReply
+    });
+}
+
+function ProcessReply(reply) {
+    console.log(reply)
+    cs = reply.cs;
+    var utterThis = new SpeechSynthesisUtterance(reply.output);
+    console.log(synth.getVoices());
+    var cleverOutput = document.createElement("p");
+    cleverOutput.innerHTML = reply.output;
+    utterThis.voice = voices[5];
+    synth.speak(utterThis);
+
+    textContainer.appendChild(cleverOutput);
+    recognition.start();
+}
+
