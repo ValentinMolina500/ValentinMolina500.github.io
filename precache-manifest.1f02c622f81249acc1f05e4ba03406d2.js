f98d64817d5baf77e73071dbc067521cc0efa656
Valentin Mollina <ValentinMolina500>
2020-01-01 13:03:28 -0800

    Updates

---
+++ precache-manifest.1f02c622f81249acc1f05e4ba03406d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59225716284f12dfb7b35515a0196e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "637a0edffe5e00b7c3c0",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "637a0edffe5e00b7c3c0",
+    "url": "/static/js/main.69d39631.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
